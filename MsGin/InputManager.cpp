c677af02e81d6c6f0a280ec3494495908a45a225
deliewout <deliewout@gmail.com>
2025-03-01 16:37:49 +0100

    added imgui

--- MsGin/InputManager.cpp
+++ MsGin/InputManager.cpp
@@ -1,6 +1,9 @@
 #include <SDL.h>
 #include "InputManager.h"
 
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+
 bool dae::InputManager::ProcessInput()
 {
 	SDL_Event e;
@@ -14,6 +17,8 @@ bool dae::InputManager::ProcessInput()
 		if (e.type == SDL_MOUSEBUTTONDOWN) {
 			
 		}
+
+		ImGui_ImplSDL2_ProcessEvent(&e);
 		// etc...
 	}
 
