f6b6bf3c24b1537b760272e29e497df5a62ae8e4
deliewout <deliewout@gmail.com>
2025-03-30 22:22:39 +0200

    converted engine to static lib

---
+++ MsPacMan/MsPacMan.cpp
@@ -0,0 +1,90 @@
+#if _DEBUG
+#if __has_include(<vld.h>)
+#include <vld.h>
+#endif
+#endif
+
+#include "FPSComponent.h"
+#include "GameObject.h"
+#include "Minigin.h"
+#include "SceneManager.h"
+#include "ResourceManager.h"
+#include "TextObject.h"
+#include "Scene.h"
+#include "GameObject.h"
+#include "HealthComponent.h"
+#include "InputManager.h"
+#include "RenderComponent.h"
+#include "TrashTheCache.h"
+
+void load()
+{
+	auto& scene = dae::SceneManager::GetInstance().CreateScene("Demo");
+	auto& input = dae::InputManager::GetInstance();
+
+	const float movementSpeed{ 2.f };
+
+	auto backGround = std::make_shared<dae::GameObject>();
+	auto texture = dae::ResourceManager::GetInstance().LoadTexture("background.tga");
+	auto newTexture = backGround->AddComponent<dae::RenderComponent>();
+	newTexture->Settexture(texture);
+	scene.Add(backGround);
+
+	auto logo = std::make_shared<dae::GameObject>();
+	texture = dae::ResourceManager::GetInstance().LoadTexture("logo.tga");
+	newTexture = logo->AddComponent<dae::RenderComponent>();
+	newTexture->Settexture(texture);
+	logo->GetTransform()->SetLocalPos(216, 180);
+	scene.Add(logo);
+
+	auto text = std::make_shared<dae::GameObject>();
+	auto font = dae::ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
+	auto to = text->AddComponent<dae::TextObject>();
+	to->SetFont(font);
+	to->SetText("Programming 4 assignment");
+	text->GetTransform()->SetLocalPos(80, 20);
+	scene.Add(text);
+
+	auto fps = std::make_shared<dae::GameObject>();
+	fps->GetTransform()->SetLocalPos(20, 60);
+	font = dae::ResourceManager::GetInstance().LoadFont("Lingua.otf", 20);
+	auto tc = fps->AddComponent<dae::TextObject>();
+	tc->SetFont(font);
+	tc->SetText("null");
+	fps->AddComponent<dae::FPSComponent>();
+	scene.Add(fps);
+
+	auto MsPacMan = std::make_shared<dae::GameObject>();
+	texture = dae::ResourceManager::GetInstance().LoadTexture("Sprites/MsPacman.jpg");
+	newTexture = MsPacMan->AddComponent<dae::RenderComponent>();
+	newTexture->Settexture(texture);
+	MsPacMan->GetTransform()->SetLocalPos(80, 140);
+	MsPacMan->AddComponent<dae::HealthComponent>();
+	scene.Add(MsPacMan);
+
+	input.BindGamePadCommand(0, dae::GamepadButtons::Dpad_Left, dae::KeyStates::down, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ -1,0 }, movementSpeed));
+	input.BindGamePadCommand(0, dae::GamepadButtons::Dpad_Right, dae::KeyStates::down, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ 1,0 }, movementSpeed));
+	input.BindGamePadCommand(0, dae::GamepadButtons::Dpad_Down, dae::KeyStates::down, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ 0,-1 }, movementSpeed));
+	input.BindGamePadCommand(0, dae::GamepadButtons::Dpad_Up, dae::KeyStates::down, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ 0,1 }, movementSpeed));
+
+	auto Pacman = std::make_shared<dae::GameObject>();
+	texture = dae::ResourceManager::GetInstance().LoadTexture("Sprites/Pacman.jpg");
+	newTexture = Pacman->AddComponent<dae::RenderComponent>();
+	newTexture->Settexture(texture);
+	Pacman->GetTransform()->SetLocalPos(100, 140);
+	scene.Add(Pacman);
+
+	input.BindKeyBoardCommand(SDL_SCANCODE_LEFT, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ -1,0 }, 2 * movementSpeed));
+	input.BindKeyBoardCommand(SDL_SCANCODE_RIGHT, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ 1,0 }, 2 * movementSpeed));
+	input.BindKeyBoardCommand(SDL_SCANCODE_DOWN, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ 0,-1 }, 2 * movementSpeed));
+	input.BindKeyBoardCommand(SDL_SCANCODE_UP, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ 0,1 }, 2 * movementSpeed));
+	//auto trashTheCacheObject = std::make_shared<dae::GameObject>();
+	//trashTheCacheObject->AddComponent<dae::TrashTheCache>();
+	//scene.Add(trashTheCacheObject);
+}
+
+int main(int, char* []) {
+	dae::Minigin engine("../Data/");
+	engine.Run(load);
+	return 0;
+}
