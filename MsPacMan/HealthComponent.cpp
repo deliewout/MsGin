a7b8c278c9a91ec85ac7b4a28c3b73d3d15bcc9e
deliewout <deliewout@gmail.com>
2025-06-08 22:02:21 +0200

    start of healthcomponent

--- MsPacMan/HealthComponent.cpp
+++ MsPacMan/HealthComponent.cpp
@@ -1,1 +1,11 @@
 #include "HealthComponent.h"
+#include "Events.h"
+
+void HealthComponent::RemoveHealth(int )
+{
+}
+
+void HealthComponent::IsOutOfLives()
+{
+	//m_Subject->NotifyObservers(dae::Events::PlayerDies);
+}
--- MsPacMan/HealthComponent.h
+++ MsPacMan/HealthComponent.h
@@ -1,18 +1,27 @@
 #pragma once
 #include "Component.h"
+#include "Subject.h"
+#include <memory>
 
-namespace dae
+class HealthComponent : public dae::Component, dae::Subject
 {
-	class HealthComponent : public Component
-	{
-	public:
-		HealthComponent(GameObject* pOwner) :Component(pOwner){}
-		~HealthComponent() = default;
-		HealthComponent(const HealthComponent& other) = delete;
-		HealthComponent(HealthComponent&& other) = delete;
-		HealthComponent& operator=(const HealthComponent& other) = delete;
-		HealthComponent& operator=(HealthComponent&& other) = delete;
-	};
-}
+public:
+	HealthComponent(dae::GameObject* pOwner, int lives) :Component(pOwner),m_Lives(lives){}
+	~HealthComponent() = default;
+	HealthComponent(const HealthComponent& other) = delete;
+	HealthComponent(HealthComponent&& other) = delete;
+	HealthComponent& operator=(const HealthComponent& other) = delete;
+	HealthComponent& operator=(HealthComponent&& other) = delete;
+
+
+	void RemoveHealth(int lives);
+	void IsOutOfLives();
+
+private:
+	int m_Lives;
+	std::unique_ptr<dae::Subject> m_Subject;
+};
+
+
 
 
