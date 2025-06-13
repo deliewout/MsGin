ec54ed1f3debf41499cfa848f37d8cc9ab13018d
deliewout <deliewout@gmail.com>
2025-06-13 19:08:19 +0200

    Added a first level
    its completly empry except for the level itself that is loaded

--- MsPacMan/MsPacMan.cpp
+++ MsPacMan/MsPacMan.cpp
@@ -18,31 +18,60 @@
 #include "PlayerMoveCommand.h"
 #include "SDLSoundSystem.h"
 #include "LogSoundSystem.h"
+#include "MuteCommand.h"
+#include "LevelCommands.h"
+#include "LevelSelector.h"
 
-void load()
+void LoadStartScreen()
 {
-	auto& scene = dae::SceneManager::GetInstance().CreateScene("Demo");
+	auto& scene = dae::SceneManager::GetInstance().CreateScene("StartScreen");
+	//auto& input = dae::InputManager::GetInstance();
+
+	//SDL_GetWindowSize()
+
+	auto logo = std::make_shared<dae::GameObject>();
+	auto texture = dae::ResourceManager::GetInstance().LoadTexture("Logo.jpg");
+	auto render = logo->AddComponent<dae::RenderComponent>();
+	logo->GetTransform()->SetLocalPos(80, 80);
+	logo->GetTransform()->SetWorldScale(2.f, 2.f);
+	render->Settexture(texture);
+	
+	scene.Add(logo);
+
+	auto player1 = std::make_shared<dae::GameObject>();
+	texture = dae::ResourceManager::GetInstance().LoadTexture("1Player.jpg");
+	render = player1->AddComponent<dae::RenderComponent>();
+	player1->GetTransform()->SetLocalPos(160, 240);
+	player1->GetTransform()->SetWorldScale(2.f, 2.f);
+	render->Settexture(texture);
+
+	scene.Add(player1);
+
+	auto player2 = std::make_shared<dae::GameObject>();
+	texture = dae::ResourceManager::GetInstance().LoadTexture("2Player.jpg");
+	render = player2->AddComponent<dae::RenderComponent>();
+	player2->GetTransform()->SetLocalPos(160, 280);
+	player2->GetTransform()->SetWorldScale(2.f, 2.f);
+	render->Settexture(texture);
+
+	scene.Add(player2);
+
+	auto versus = std::make_shared<dae::GameObject>();
+	texture = dae::ResourceManager::GetInstance().LoadTexture("Versus.jpg");
+	render = versus->AddComponent<dae::RenderComponent>();
+	versus->GetTransform()->SetLocalPos(170, 320);
+	versus->GetTransform()->SetWorldScale(2.f, 2.f);
+	render->Settexture(texture);
+
+	scene.Add(versus);
+
+}
+void LoadFirstLevel()
+{
+	auto& scene = dae::SceneManager::GetInstance().CreateScene("Level1");
 	auto& input = dae::InputManager::GetInstance();
-#if _DEBUG
-	dae::ServiceLocator::RegisterSoundSystem(std::make_unique<dae::LogSoundSystem>(std::make_unique<dae::SDLSoundSystem>()));
-#else
-	dae::ServiceLocator::RegisterSoundSystem(std::make_unique<dae::SDLSoundSystem>());
-#endif
 
 	const float movementSpeed{ 2.f };
-
-	//auto backGround = std::make_shared<dae::GameObject>();
-	//auto texture = dae::ResourceManager::GetInstance().LoadTexture("background.tga");
-	//auto newTexture = backGround->AddComponent<dae::RenderComponent>();
-	//newTexture->Settexture(texture);
-	//scene.Add(backGround);
-
-	//auto logo = std::make_shared<dae::GameObject>();
-	//texture = dae::ResourceManager::GetInstance().LoadTexture("logo.tga");
-	//newTexture = logo->AddComponent<dae::RenderComponent>();
-	//newTexture->Settexture(texture);
-	//logo->GetTransform()->SetLocalPos(216, 180);
-	//scene.Add(logo);
 
 	//auto text = std::make_shared<dae::GameObject>();
 	//auto font = dae::ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
@@ -64,8 +93,8 @@ void load()
 	auto level = std::make_shared<dae::GameObject>();
 	auto texture = dae::ResourceManager::GetInstance().LoadTexture("Level/FirstLevel.jpg");
 	auto newTexture = level->AddComponent<dae::RenderComponent>();
-	level->GetTransform()->MoveLocalPosition({180,120});
-	level->GetTransform()->SetWorldScale(1.5f, 1.5f);
+	level->GetTransform()->SetLocalPos({ 0,80 });
+	level->GetTransform()->SetWorldScale(2.5f, 2.5f);
 	newTexture->Settexture(texture);
 	scene.Add(level);
 
@@ -75,7 +104,7 @@ void load()
 	newTexture = MsPacMan->AddComponent<dae::RenderComponent>();
 	newTexture->Settexture(texture);
 	MsPacMan->GetTransform()->SetLocalPos(80, 140);
-	MsPacMan->AddComponent<HealthComponent>( 3);
+	MsPacMan->AddComponent<HealthComponent>(3);
 	scene.Add(MsPacMan);
 
 	input.BindGamePadCommand(0, dae::GamepadButtons::Dpad_Left, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ -1,0 }, movementSpeed));
@@ -83,25 +112,43 @@ void load()
 	input.BindGamePadCommand(0, dae::GamepadButtons::Dpad_Down, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ 0,1 }, movementSpeed));
 	input.BindGamePadCommand(0, dae::GamepadButtons::Dpad_Up, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(MsPacMan, glm::vec2{ 0,-1 }, movementSpeed));
 
-	//auto Pacman = std::make_shared<dae::GameObject>();
-	//texture = dae::ResourceManager::GetInstance().LoadTexture("Sprites/Pacman.jpg");
-	//newTexture = Pacman->AddComponent<dae::RenderComponent>();
-	//newTexture->Settexture(texture);
-	//Pacman->GetTransform()->SetLocalPos(100, 140);
-	//scene.Add(Pacman);
+	input.BindKeyBoardCommand(SDL_SCANCODE_F2, dae::KeyStates::down, std::make_unique<MuteCommand>());
 
-	//input.BindKeyBoardCommand(SDL_SCANCODE_LEFT, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(Pacman, glm::vec2{ -1,0 },  movementSpeed));
-	//input.BindKeyBoardCommand(SDL_SCANCODE_RIGHT, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(Pacman, glm::vec2{ 1,0 },  movementSpeed));
-	//input.BindKeyBoardCommand(SDL_SCANCODE_DOWN, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(Pacman, glm::vec2{ 0,1 },  movementSpeed));
-	//input.BindKeyBoardCommand(SDL_SCANCODE_UP, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(Pacman, glm::vec2{ 0,-1 },  movementSpeed));
+	auto Pacman = std::make_shared<dae::GameObject>();
+	texture = dae::ResourceManager::GetInstance().LoadTexture("Sprites/Pacman.jpg");
+	newTexture = Pacman->AddComponent<dae::RenderComponent>();
+	newTexture->Settexture(texture);
+	Pacman->GetTransform()->SetLocalPos(100, 140);
+	scene.Add(Pacman);
+
+	input.BindKeyBoardCommand(SDL_SCANCODE_LEFT, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(Pacman, glm::vec2{ -1,0 }, movementSpeed));
+	input.BindKeyBoardCommand(SDL_SCANCODE_RIGHT, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(Pacman, glm::vec2{ 1,0 }, movementSpeed));
+	input.BindKeyBoardCommand(SDL_SCANCODE_DOWN, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(Pacman, glm::vec2{ 0,1 }, movementSpeed));
+	input.BindKeyBoardCommand(SDL_SCANCODE_UP, dae::KeyStates::pressed, std::make_unique<PlayerMoveCommand>(Pacman, glm::vec2{ 0,-1 }, movementSpeed));
+}
+
+void load()
+{
 	
-	//auto trashTheCacheObject = std::make_shared<dae::GameObject>();
-	//trashTheCacheObject->AddComponent<dae::TrashTheCache>();
-	//scene.Add(trashTheCacheObject);
+	auto& input = dae::InputManager::GetInstance();
+#if _DEBUG
+	dae::ServiceLocator::RegisterSoundSystem(std::make_unique<dae::LogSoundSystem>(std::make_unique<dae::SDLSoundSystem>()));
+#else
+	dae::ServiceLocator::RegisterSoundSystem(std::make_unique<dae::SDLSoundSystem>());
+#endif
+	LevelSelector levelSelector;
+	levelSelector.AddLevel("StartScreen");
+	levelSelector.AddLevel("Level1");
+
+	LoadStartScreen();
+	LoadFirstLevel();
+
 
 	auto& soundSystem = dae::ServiceLocator::GetSoundSystem();
-	soundSystem.LoadMusic("Sounds/ms_start.wav");
+	soundSystem.SetVolume(10);
+	soundSystem.LoadMusic("Sounds/ms_fright.wav");
 
+	input.BindKeyBoardCommand(SDL_SCANCODE_F2, dae::KeyStates::pressed,std::make_unique<SkipLevelCommand>(levelSelector));
 }
 
 int main(int, char* []) {
